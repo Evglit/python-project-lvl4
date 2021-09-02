2f5263a05d859065831defb36df846bc51e60b61
Evgeny <litun-07@mail.ru>
2021-09-02 22:13:15 +0300

    Test rollbar 2

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.LabelListPage.as_view(), name='labels'),
     path('create/', views.CreateLabel.as_view(), name='create_label'),
-    #path('<int:pk>/update/', views.UpdateLabel.as_view(), name='update_label'),
+    path('<int:pk>/update/', views.UpdateLabel.as_view(), name='update_label'),
     path('<int:pk>/delete/', views.DeleteLabel.as_view(), name='delete_label')
 ]
