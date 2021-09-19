2f27d2f3b4875d45de382612646a67c425e44c25
Evgeny <litun-07@mail.ru>
2021-09-19 15:39:35 +0300

    Changed settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=0, ssl_require=False)
-django_heroku.settings(locals())
+django_heroku.settings(locals() ,databases=False)
 
 
 # Password validation
